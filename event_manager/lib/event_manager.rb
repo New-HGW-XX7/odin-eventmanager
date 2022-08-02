2c432ab26959e9e8347b0966588f19434124fadd
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-08-02 15:18:02 +0200

    Complete iteration 0

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,15 @@
+puts 'Event Manager initialized'
+
+#contents = File.read('event_attendees.csv')
+#puts contents
+
+### Iteration 0
+# lines = File.readlines('event_attendees.csv')
+# lines.each_with_index do |line, index|
+#   next if index == 0
+#   columns = line.split(',')
+#   name = columns[2]
+#   puts name
+# end
+
+### Iteraion 1
