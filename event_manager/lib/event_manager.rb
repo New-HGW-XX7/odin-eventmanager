c3edd4328fd45c69f063965c65ff73dcf2469847
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-08-02 15:23:41 +0200

    Complete iteration 1

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,4 +1,5 @@
 puts 'Event Manager initialized'
+require 'csv'
 
 #contents = File.read('event_attendees.csv')
 #puts contents
@@ -13,3 +14,17 @@ puts 'Event Manager initialized'
 # end
 
 ### Iteraion 1
+# require 'csv'
+# contents = CSV.open(
+#   'event_attendees.csv',
+#   headers: true,
+#   header_converters: :symbol
+# )
+
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = row[:zipcode]
+#   puts "#{name} #{zipcode}"
+# end
+
+### Iteration 2
