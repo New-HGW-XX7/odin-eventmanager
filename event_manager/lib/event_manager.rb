f0a59880a812966749d08f57a337c098904be421
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-08-05 18:01:15 +0200

    Complete iteration 4

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,6 +1,7 @@
 puts 'Event Manager initialized'
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 #contents = File.read('event_attendees.csv')
 #puts contents
@@ -27,16 +28,10 @@ require 'google/apis/civicinfo_v2'
 #   puts "#{name} #{zipcode}"
 # end
 
-### Iteration 2 and 3
+### Iteration 2 and 3 and 4
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
-
-contents = CSV.open(
-  '../event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -47,23 +42,49 @@ def legislators_by_zipcode(zip)
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    ).officials # Iteration 4
+
+    # legislators = legislators.officials
+    # legislator_names = legislators.map(&:name)  Iteration 3
+    # legislator_names.join(", ")
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+
+  #puts "#{name} #{zipcode} #{legislators}"
 end
 
 
